86c0252372806d71c0b2f4f3426168170665db13
Guillermo A. Perez <gaperez64@gmail.com>
2023-09-04 19:58:40 +0200

    tags work

--- confest.py
+++ confest.py
@@ -4,6 +4,34 @@ import pandas as pd
 import sys
 
 
+def regdfor(row):
+    regd = []
+    if (row["Student_pre"] or
+            row["Student_pre_early"] or
+            row["Regular_pre"] or
+            row["Regular_pre_early"]):
+        regd.append("B")
+    if (row["Student_reception"] == "yes" or
+            row["Regular_reception"] == "yes"):
+        regd.append("R")
+    if (row["Student"] or
+            row["Student_early"] or
+            row["Regular"] or
+            row["Regular_early"]):
+        regd.append("C")
+    if (row["Student_extra_yes"] or
+            row["Student_extra_yes_early"] or
+            row["Regular_extra_yes"] or
+            row["Regular_extra_yes_early"]):
+        regd.append("X")
+    if (row["Student_post"] or
+            row["Student_post_early"] or
+            row["Regular_post"] or
+            row["Regular_post_early"]):
+        regd.append("A")
+    return " ".join(regd)
+
+
 def process(fname):
     frame = pd.read_csv(fname, header=0, sep=';', quotechar='"')
 
@@ -34,8 +62,57 @@ def process(fname):
     # Restricting to payed status now
     print("# Paying registrants")
     frame = frame[frame["MultiSafePayStatus"] == "completed"]
+    # Having a single name field will come in handy
+    frame["Fullname"] = frame.apply(lambda row:
+                                    row["Firstname"] + " " + row["Familyname"],
+                                    axis=1)
     print(f"No. of participants who paid already: {frame.shape[0]}")
 
+    # Dumping to file the information for tags
+    f = open("parts.tex", "w")
+    i = 0
+    for _, row in frame.iterrows():
+        if i % 10 == 0:
+            f.write("\\tagpage")
+        if i % 2 == 0:
+            (name1, affil1) = (row["Fullname"],
+                               row["Affiliation"].replace("&", "\\&"))
+            regd1 = regdfor(row)
+        else:
+            (name2, affil2) = (row["Fullname"],
+                               row["Affiliation"].replace("&", "\\&"))
+            regd2 = regdfor(row)
+            f.write("{"
+                    f"{{{name1}}}/{{{affil1}}}/{{{regd1}}}/"
+                    f"{{{name2}}}/{{{affil2}}}/{{{regd2}}}"
+                    "}%\n")
+        i += 1
+    print(f"Value of i before mod = {i}, {i % 10}")
+    if i % 10 != 0:
+        while i % 10 != 0:
+            if i % 2 == 0:
+                (name1, affil1) = ("", "")
+                regd1 = ""
+            else:
+                (name2, affil2) = ("", "")
+                regd2 = ""
+                f.write("{"
+                        f"{{{name1}}}/{{{affil1}}}/{{{regd1}}}/"
+                        f"{{{name2}}}/{{{affil2}}}/{{{regd2}}}"
+                        "}%\n")
+            i += 1
+    f.close
+
+    # Dumping to file the food restrictions of paying registrants
+    frame["Diet restr"] = frame.apply(lambda row:
+                                      row["Diet"] if
+                                      row["Diet"] != "Diet_other"
+                                      else row["Diet_other"],
+                                      axis=1)
+    food = frame[["Fullname", "Diet restr"]]
+    food.to_csv("diet.csv")
+
+    # Information about money
     total = frame["Bedrag"].sum()
     print(f"Total amount received: {total}eur")
 
